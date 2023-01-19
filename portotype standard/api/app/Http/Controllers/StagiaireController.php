d007dc938ad5c60a09f67b8c72bab2ae167a8972
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2023-01-19 23:27:13 +0100

    prototype standard

---
+++ portotype standard/api/app/Http/Controllers/StagiaireController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stagiaire;
+use Illuminate\Http\Request;
+
+class StagiaireController extends Controller
+{
+    //
+    public function search($nom)
+    {
+    return Stagiaire::where('nom', 'like', $nom.'%')->get();
+    }
+}
