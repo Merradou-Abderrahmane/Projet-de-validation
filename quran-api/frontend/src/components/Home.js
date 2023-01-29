dbce9b1cfe323ca0f91163f628dbced6c2a991a0
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2023-01-30 00:34:20 +0100

    consume audio from one qari

--- quran-api/frontend/src/components/Home.js
+++ quran-api/frontend/src/components/Home.js
@@ -15,53 +15,54 @@ class Home extends Component {
         .then(res => {
             const surahs = res.data.data;
             this.setState({ surahs });
-        })
-    }
+        })        
+        }       
 
-    playSurah(surah) {
-        const audio = document.querySelector('.quranPlayer');
-        audio.src = `http://api.alquran.cloud/v1/surah/${surah.number}/ar.alafasy`;
-        audio.onloadeddata = () => {
-            audio.play();
-            console.log(audio);
-        }
-        // console log to see if audio is playing
-
-
-        
-        }
-
+        playSurah(surahNumber) {
+            const audioPlayer = document.querySelector('.quranPlayer');
+            const paddedSurahNumber = `00${surahNumber}`.slice(-3);
+            audioPlayer.src = `https://server11.mp3quran.net/a_ahmed/${paddedSurahNumber}.mp3`;
+            audioPlayer.play();
+          }
+              
     // 
     
     render() {
         return (
-
-        <div>
-             <div className="container">
-            <div className="player">
+          <div>
+            <div className="container">
+              <div className="player">
                 <div className="ayah">اضغط علي السورة للاستماع اليها</div>
-                <audio src="" className="quranPlayer" controls  autoPlay></audio>
+                <audio src={this.state.surahAudio} className="quranPlayer" controls  autoPlay></audio>
                 <div className="buttons">
-                    <div className="icon next"><i className="fas fa-forward"></i></div>
-                    <div className="icon play"><i className="fas fa-play"></i></div>
-                    <div className="icon prev"><i className="fas fa-backward"></i></div>
+                  <div className="icon next">
+                    <i className="fas fa-forward"></i>
+                  </div>
+                  <div className="icon play">
+                    <i className="fas fa-play"></i>
+                  </div>
+                  <div className="icon prev">
+                    <i className="fas fa-backward"></i>
+                  </div>
                 </div>
-            </div>
-            <div className="surahs">
+              </div>
+              <div className="surahs">
                 {this.state.surahs.map(surah => (
-                    <div className="surah-container" onClick={() => this.playSurah(surah)}>
-                        <div className="surah-name">{surah.name} - {surah.englishNameTranslation}</div>
-                        <div className="favorite-icon">
-                            <i className="fas fa-heart"></i>
-                        </div>
+                  <div className="surah-container" onClick={() => this.playSurah(surah.number)}>
+                    <div className="surah-name">
+                      {surah.name} - {surah.englishNameTranslation}
                     </div>
+                    <div className="favorite-icon">
+                      <i className="fas fa-heart"></i>
+                    </div>
+                  </div>
                 ))}
+              </div>
             </div>
-            </div>
-
-        </div>
-        )
+          </div>
+        );
+      }
     }
-    }
+    
 
 export default Home;
